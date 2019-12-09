56cded9e9236b9985e80077e03e6f838460903c4
Oyetunji Temitayo <tytunji29@gmail.com>
2019-12-09 12:24:40 +0100

    fixed

---
+++ TestingRepo/Test.API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TestingRepo/TestingRepo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
